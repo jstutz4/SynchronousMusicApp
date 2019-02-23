92f5838d41bb8b12686ffaf7a6074882f49093c9
Jon STuTZ <stu15012@byui.edu>
2019-02-23 11:21:10 -0700

    Added join music group method and activity Added hosting music activity. Added basic UI.

--- app/src/main/java/com/example/synchronousmusicapp/JoinGroup.java
+++ app/src/main/java/com/example/synchronousmusicapp/JoinGroup.java
@@ -1,7 +1,9 @@
 package com.example.synchronousmusicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class JoinGroup extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class JoinGroup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_group);
+    }
+
+    public void playMusic(View view) {
+        Intent playMusic = new Intent(this,PlayMusic.class);
+        startActivity(playMusic);
     }
 }
