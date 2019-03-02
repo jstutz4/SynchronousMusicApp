70ac50187f96870ac5e79c0c43a1656438428a60
Jon STuTZ <stu15012@byui.edu>
2019-03-02 16:01:09 -0700

    first attempt to get the discover service client side to work, code has errors that are commented out.

--- app/src/main/java/com/example/synchronousmusicapp/JoinGroup.java
+++ app/src/main/java/com/example/synchronousmusicapp/JoinGroup.java
@@ -1,12 +1,15 @@
 package com.example.synchronousmusicapp;
 
 import android.content.Intent;
+import android.net.nsd.NsdManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class JoinGroup extends AppCompatActivity {
     public String testID = "testID101";
+    private RequestService discoveryListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,8 @@ public class JoinGroup extends AppCompatActivity {
         Intent playMusic = new Intent(this,PlayMusic.class);
         startActivity(playMusic);
     }
+
+
 
     public Boolean checkGroupId(String id){
       return id.equals(testID);
