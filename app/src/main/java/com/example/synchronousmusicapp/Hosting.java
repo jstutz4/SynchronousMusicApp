26d45e7b8cd303bc4fceb190683437576e02dcb0
Jack Walls <josejoaquinparedes0@gmail.com>
2019-03-08 21:10:01 -0700

    Hosting is set up, will need to test and set media browser for onRegisteredService().

--- app/src/main/java/com/example/synchronousmusicapp/Hosting.java
+++ app/src/main/java/com/example/synchronousmusicapp/Hosting.java
@@ -1,13 +1,83 @@
 package com.example.synchronousmusicapp;
 
+import android.content.Context;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Hosting extends AppCompatActivity {
 
+        private NsdManager.RegistrationListener registrationListener;
+        private NsdManager nsdManager;
+        private Context context;
+        private String serviceName;
+        private int LocalPort;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hosting);
     }
+    public void registerService(Context context) throws IOException {
+
+        int port = findOpenSocket();
+
+        // Create the NsdServiceInfo object, and populate it.
+        NsdServiceInfo serviceInfo = new NsdServiceInfo();
+
+        // The name is subject to change based on conflicts
+        // with other services advertised on the same network.
+        serviceInfo.setServiceName("SynchMusic");
+        serviceInfo.setServiceType("_http._tcp");
+        serviceInfo.setPort(port);
+
+        nsdManager = (NsdManager)context.getSystemService(Context.NSD_SERVICE);
+
+        nsdManager.registerService(
+                serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener);
+
+    }
+
+    private int findOpenSocket() throws java.io.IOException {
+        // Initialize a server socket on the next available port.
+        ServerSocket serverSocket = new ServerSocket(0);
+
+        // Store the chosen port.
+        LocalPort =  serverSocket.getLocalPort();
+        serverSocket.close();
+
+        return LocalPort;
+    }
+
+    public void initializeRegistrationListener() {
+        registrationListener = new NsdManager.RegistrationListener() {
+            @Override
+            public void onServiceRegistered(NsdServiceInfo NsdServiceInfo) {
+                // Save the service name. Android may have changed it in order to
+                // resolve a conflict, so update the name you initially requested
+                // with the name Android actually used.
+                serviceName = NsdServiceInfo.getServiceName();
+            }
+
+            @Override
+            public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Registration failed! Put debugging code here to determine why.
+            }
+
+            @Override
+            public void onServiceUnregistered(NsdServiceInfo arg0) {
+                // Service has been unregistered. This only happens when you call
+                // NsdManager.unregisterService() and pass in this listener.
+            }
+
+            @Override
+            public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Unregistration failed. Put debugging code here to determine why.
+            }
+        };
+    }
+
 }
