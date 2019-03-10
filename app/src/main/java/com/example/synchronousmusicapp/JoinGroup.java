7a0836daa5ba3d59710487d30688fc5f3c3e9853
Jon STuTZ <stu15012@byui.edu>
2019-03-09 17:43:57 -0700

    almost done I think it will mostly work when service is broadcasting

--- app/src/main/java/com/example/synchronousmusicapp/JoinGroup.java
+++ app/src/main/java/com/example/synchronousmusicapp/JoinGroup.java
@@ -20,14 +20,14 @@ import java.net.ServerSocket;
 public class JoinGroup extends AppCompatActivity {
     //public static interface WifiP2pManager.DnsSdServiceResponseListener;
     public String testID = "testID101";
-    WifiP2pManager manager;
-    WifiP2pManager.Channel channel;
+//    WifiP2pManager manager;
+//    WifiP2pManager.Channel channel;
     Activity a = this;
-    ServiceDiscovery serviceDiscovery;
-    // not sure how to figure this line out?
-    android.net.wifi.p2p.WifiP2pDevice mphone;
-    WifiP2pDnsSdServiceRequest serviceRequest;
-    android.net.wifi.p2p.WifiP2pManager.ChannelListener channelListener;
+//    ServiceDiscovery serviceDiscovery;
+//    // not sure how to figure this line out?
+//    android.net.wifi.p2p.WifiP2pDevice mphone;
+//    WifiP2pDnsSdServiceRequest serviceRequest;
+//    android.net.wifi.p2p.WifiP2pManager.ChannelListener channelListener;
 
 
     //nsd manager
@@ -68,17 +68,18 @@ public class JoinGroup extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        this.initializeDiscoveryListener();
-        this.initializeResolveListener();
-        manager2.discoverServices("_http._tcp",NsdManager.PROTOCOL_DNS_SD,discoveryListener);
-        manager2.resolveService(serviceInfo,resolveListener);
+
+        initializeDiscoveryListener();
+        initializeResolveListener();
+        discoverServices("_http._tcp",NsdManager.PROTOCOL_DNS_SD,discoveryListener);
+        //manager2.resolveService(serviceInfo,resolveListener);
 
     }
 
     @Override
     protected void onPause() {
-        if (nsdHelper != null) {
-            nsdHelper.tearDown();
+        if (this != null) {
+            this.tearDown();
         }
         super.onPause();
     }
@@ -86,14 +87,14 @@ public class JoinGroup extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if (nsdHelper != null) {
-            nsdHelper.discoverServices();
+        if (this != null) {
+            this.discoverServices("_http._tcp",NsdManager.PROTOCOL_DNS_SD,discoveryListener);
         }
     }
 
     @Override
     protected void onDestroy() {
-        nsdHelper.tearDown();
+        this.tearDown();
         connection.tearDown();
         super.onDestroy();
     }
@@ -185,7 +186,7 @@ public class JoinGroup extends AppCompatActivity {
                     Log.d(TAG, "resolve Succeeded " + serviceInfo);
                     if(serviceInfo.getServiceName().equals("SynchMusic")){
                         Log.d(TAG, "Same IP");
-                        
+
                     }
                     //Not sure what this does or if we need to duplicate code from the host.jave
 //                    mService = serviceInfo;
@@ -201,71 +202,67 @@ public class JoinGroup extends AppCompatActivity {
         startActivity(playMusic);
     }
 
-    public void addServiceRequest(){
-        serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
-        manager.addServiceRequest(channel,
-                serviceRequest,
-                new WifiP2pManager.ActionListener() {
-                    @Override
-                    public void onSuccess() {
-                        // Success!
-                        Toast.makeText(a, " add service request Succuss", Toast.LENGTH_LONG).show();
+//    public void addServiceRequest(){
+//        serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
+//        manager.addServiceRequest(channel,
+//                serviceRequest,
+//                new WifiP2pManager.ActionListener() {
+//                    @Override
+//                    public void onSuccess() {
+//                        // Success!
+//                        Toast.makeText(a, " add service request Succuss", Toast.LENGTH_LONG).show();
+//
+//                    }
+//
+//                    @Override
+//                    public void onFailure(int code) {
+//                        // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+//                        if (code == WifiP2pManager.P2P_UNSUPPORTED) {
+//                            Log.d("Synch Music", "P2P isn't supported on this device.");
+//                        }
+//                        else{
+//                            Toast.makeText(a, "add service request went wrong", Toast.LENGTH_LONG).show();
+//
+//                        }
+//                    }
+//                });
+//    }
 
-                    }
-
-                    @Override
-                    public void onFailure(int code) {
-                        // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
-                        if (code == WifiP2pManager.P2P_UNSUPPORTED) {
-                            Log.d("Synch Music", "P2P isn't supported on this device.");
-                        }
-                        else{
-                            Toast.makeText(a, "add service request went wrong", Toast.LENGTH_LONG).show();
-
-                        }
-                    }
-                });
+    public void discoverServices(String serviceType, int protocol, NsdManager.DiscoveryListener listener){
+            stopDiscovery();
+            initializeDiscoveryListener();
+            discoverServices("_http._tcp",NsdManager.PROTOCOL_DNS_SD,discoveryListener);
     }
 
-    public void discoverServices(){
-        manager.discoverServices(channel, new WifiP2pManager.ActionListener() {
 
-            @Override
-            public void onSuccess() {
-                // Success!
-                Toast.makeText(a, "Succuss, now start receiving music stream", Toast.LENGTH_LONG).show();
-
-
+    public void stopDiscovery() {
+        if (discoveryListener != null) {
+            try {
+                stopServiceDiscovery(discoveryListener);
+            } finally {
             }
-
-            @Override
-            public void onFailure(int code) {
-                // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
-                if (code == WifiP2pManager.P2P_UNSUPPORTED) {
-                    Log.d("Synch Music", "P2P isn't supported on this device.");
-                }
-                else{
-                    Toast.makeText(a, "discover services went wrong", Toast.LENGTH_LONG).show();
-
-                }
-            }
-        });
+            discoveryListener = null;
+        }
     }
 
-    public void discoverPeers(){
-        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {
-                Toast.makeText(a, "Succuss, found peers", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onFailure(int reason) {
-                Toast.makeText(a, " peers Failer achived Way to Go!", Toast.LENGTH_LONG).show();
-
-            }
-        });
+    public void stopServiceDiscovery(NsdManager.DiscoveryListener listener){
+            Log.e(TAG,"Impliment this function i guess");
     }
+
+//    public void discoverPeers(){
+//        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+//            @Override
+//            public void onSuccess() {
+//                Toast.makeText(a, "Succuss, found peers", Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onFailure(int reason) {
+//                Toast.makeText(a, " peers Failer achived Way to Go!", Toast.LENGTH_LONG).show();
+//
+//            }
+//        });
+//    }
 
 
 
