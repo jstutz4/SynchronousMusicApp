0c71ba762804bd6dc019db146536fd0bc916db5e
Jon STuTZ <stu15012@byui.edu>
2019-03-21 21:04:49 -0600

    add functionality to add music from device storage and able to play the song.

--- app/src/main/java/com/example/synchronousmusicapp/PlayMusic.java
+++ app/src/main/java/com/example/synchronousmusicapp/PlayMusic.java
@@ -1,23 +1,46 @@
 package com.example.synchronousmusicapp;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AndroidException;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.ToggleButton;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
 
 /**
  * Loads previous selected song and loads the music UI to interact with the song.
  */
 public class PlayMusic extends AppCompatActivity {
-    private MediaPlayer mp;
+    private static MediaPlayer mp = new MediaPlayer();
     final static String TAG = "SynchMusic playmusic";
     String play = "play";
     String pause = "pause";
+
+    private ArrayList<String> songList;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
 
 
 
@@ -25,6 +48,7 @@ public class PlayMusic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
+        runPermission();
         Log.i(TAG, "we are running play music activity");
 
         Log.i(TAG, "mp is fine");
@@ -34,17 +58,28 @@ public class PlayMusic extends AppCompatActivity {
     }
 
     private void pauseSong() {
-        Log.i(TAG, "pause Song");
+
         if(mp.isPlaying()) {
             mp.pause();
+            Log.i(TAG, "pause Song");
         }
     }
 
     private void playSong() {
-        Log.i(TAG, "play song");
+
         if(!mp.isPlaying()) {
             mp.start();
+            Log.i(TAG, "play song");
         }
+        else{
+            mp.stop();
+        }
+    }
+    private void stopSong() {
+        if(mp.isPlaying()){
+            mp.stop();
+        }
+
     }
 
     public Boolean musicState(){
@@ -53,26 +88,157 @@ public class PlayMusic extends AppCompatActivity {
 
     /**
      * Toggles play and pause button and calls the appropriate method.
-     * @param view default
      */
     public void changeMusicState(View view) {
         Log.d(TAG, "button clicked");
+        Log.d(TAG, view.toString());
 
         Button b =  findViewById(R.id.button2);
         String states = b.getText().toString();
         String state = b.getContentDescription().toString();
         Log.i(TAG, "State is in " + state +" 2nd state is " + states.compareTo("play"));
-        if(states.compareTo("play") == 0)
-        {
-            //do something when toggle is on
-            playSong();
-            b.setText(pause);
+//        if(mp.isPlaying() && states.compareTo("play") == 0){
+//
+//        }
+//        else {
+            if (states.compareTo("play") == 0 && !mp.isPlaying()) {
+                Log.i(TAG, "song normal");
+                //do something when toggle is on
+                playSong();
+                b.setText(pause);
+            } else {
+                //do something when toggle is off
+                pauseSong();
+                b.setText(play);
+            }
         }
-        else
-        {
-            //do something when toggle is off
-            pauseSong();
-            b.setText(play);
+   // }
+
+    private void getMusic() {
+        Log.i(TAG, "getmusic started");
+        ContentResolver contentResolver = getContentResolver();
+        Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor songCursor = contentResolver.query(songUri, null,"5",null,null);
+
+
+
+        //if(songCursor != null){
+
+
+        if (songCursor != null && (songCursor).moveToFirst()) {
+            Log.i(TAG, "songCursoer 1\n " + songCursor.moveToFirst());
+
+            Log.i(TAG, "getmusic started step 1");
+            int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int songLocation = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+            int songLength = songCursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
+            do {
+                Log.i(TAG, "getmusic started step 2");
+                String currentTitle = songCursor.getString(songTitle);
+                String currentLength = songCursor.getString(songLength);
+                String currentLocation = songCursor.getString(songLocation);
+                songList.add(currentTitle + "\n" + currentLength + "\n" + currentLocation);
+                Log.i(TAG, currentTitle + "\n" + currentLength + "\n" + currentLocation);
+            } while (songCursor.moveToNext());
+        }
+        // }
+
+        songCursor.close();
+        Log.d(TAG, "getMusic ened");
+
+    }
+
+    private void runPermission() {
+        if(ContextCompat.checkSelfPermission(PlayMusic.this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED){
+            Log.i(TAG, "made it to run permission");
+            if(ActivityCompat.shouldShowRequestPermissionRationale(PlayMusic.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                ActivityCompat.requestPermissions(PlayMusic.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+            else{
+                ActivityCompat.requestPermissions(PlayMusic.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+            }
+        }
+        else{
+            Log.d(TAG, "permissions all good");
+            dostuff();
+        }
+    }
+
+
+      private void dostuff(){
+          final String[] idResource2 = new String[1];
+         final Context context = this;
+          Log.i(TAG, "context: " + context);
+        listView = findViewById(R.id.songList);
+        songList = new ArrayList<>();
+        getMusic();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, songList);
+        //if(adapter != null && listView != null) {
+            Log.i(TAG, "Adaptor not null adding something to listview");
+            listView.setAdapter(adapter);
+        //}
+        Log.i(TAG, "done adding");
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context1 = getApplicationContext();
+                String idResource = songList.get(position);
+              int pos =  idResource.indexOf("/");
+
+                idResource = idResource.substring(pos);
+//                try {
+                    playMusic(idResource);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        });
+      }
+
+    private void playMusic(String idResource) {
+        Log.d(TAG, "media player locating song");
+        if(mp.isPlaying()){
+            mp.stop();
+            mp.reset();
+        }
+        mp = MediaPlayer.create(this, Uri.parse(idResource));
+
+       //mp.setDataSource(idResource);
+       //mp.prepare();
+        if(mp != null) {
+            View view = findViewById(R.id.button2);
+//            if(mp.isPlaying()){
+//                stopSong();
+//                changeMusicState(view);
+//                Log.i(TAG, "TRY AGIAN NEW SONG");
+//                playMusic(idResource);
+//            }
+
+                Log.d(TAG, "media player is not null");
+
+                changeMusicState(view);
+
+       }
+       Log.d(TAG, "media player null");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(PlayMusic.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                        Log.d(TAG,"Permission granted!");
+                        dostuff();
+                    }
+                    else{
+                        Log.d(TAG, "NO permission granted");
+                        finish();
+                    }
+                }
+            }
         }
     }
 }
